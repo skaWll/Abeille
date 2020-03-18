c754bd9715be0a7857986b54c7ce619515de05c2
Ben <kiwihc16@free.fr>
2020-03-18 18:51:58 +0100

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -40,7 +40,7 @@
                 <div class="form-group">
                     <label class="col-lg-4 control-label" data-toggle="tooltip" title="Version Abeille.">{{Version Abeille : }}</label>
                     <div class="col-lg-4">
-                        Master
+                        Stable 19/03/2020
                     </div>
                 </div>
             </div>
